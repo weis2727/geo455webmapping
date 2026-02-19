8e0b1a487349a2bae9ec669614f44ca24f5d1f98
weis2727 <weis2727@uwlax.edu>
2026-02-19 11:51:49 -0600

    Update lab3_map.js

--- lab3/lab3_map.js
+++ lab3/lab3_map.js
@@ -83,10 +83,11 @@ var zoo = L.marker([38.92884564, -77.05014029], {icon: myIcon1}).bindPopup("<b>S
 var garden = L.marker([38.88802284, -77.01293998], {icon: myIcon5}).bindPopup("<b>United States Botanical Garden</b> <br><br><img src='images/bot_sign.JPG' width='200px'>").addTo(map);
 var portrait = L.marker([38.89771812, -77.02305119], {icon: myIcon3}).bindPopup("<b>National Portrait Gallery</b> <br><br><img src='images/port_sign.JPG' width='200px'>").addTo(map);
 var mahis = L.marker([38.89114566, -77.03007236], {icon: myIcon9}).bindPopup("<b>Smithsonian National Museum of American History</b> <br><br><img src='images/mah_dem.JPG' width='200px'>").addTo(map);
-var congress = L.marker([38.88879253, -77.00500868], {icon: myIcon6}).bindPopup("<b>Library of Congress</b> <br><br><img src='images/loc.JPG' width='200px'>").addTo(map);
+var congress = L.marker([38.88879253, -77.00500868], {icon: myIcon6}).bindPopup("<b>Library of Congress</b> <br><br><img src='images/loc.jpg' width='200px'>").addTo(map);
 var sc = L.marker([38.89065907, -77.00446126], {icon: myIcon7}).bindPopup("<b>Supreme Court</b> <br><br><img src='images/sc.JPG' width='200px'>").addTo(map);
 var capbuild = L.marker([38.88985422, -77.00908982], {icon: myIcon8}).bindPopup("<b>United States Capital Building</b> <br><br><img src='images/capitol.JPG' width='200px'>").addTo(map);
 var wh = L.marker([38.89770135, -77.03656199], {icon: myIcon2}).bindPopup("<b>The White House</b> <br><br><img src='images/white_house.JPG' width='200px'>").addTo(map);
 var washmon = L.marker([38.88946247, -77.03521437], {icon: myIcon10}).bindPopup("<b>Washington Monument</b> <br><br><img src='images/washmon.JPG' width='200px'>").addTo(map);
 var wwii = L.marker([38.88939519, -77.04054329], {icon: myIcon11}).bindPopup("<b>World War II Memorial</b> <br><br><img src='images/wwii.JPG' width='200px'>").addTo(map);
 var linc = L.marker([38.88932295, -77.05020522], {icon: myIcon12}).bindPopup("<b>Lincoln Memorial</b> <br><br><img src='images/linc.JPG' width='200px'>").addTo(map);
+
