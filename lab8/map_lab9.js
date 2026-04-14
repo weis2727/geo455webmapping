10ec368c6fb78fb0dc1ac62f91528332723f8b95
weis2727 <weis2727@uwlax.edu>
2026-04-14 11:19:22 -0500

    Add files via upload

---
+++ lab8/map_lab9.js
@@ -0,0 +1,49 @@
+var basemap = L.tileLayer('https://tile.openstreetmap.bzh/ca/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles courtesy of <a href="https://www.openstreetmap.cat" target="_blank">Breton OpenStreetMap Team</a>'
+});
+
+var mymap = L.map('map', {
+    center: [43.09157730670122, -89.41174811804763],
+    zoom: 7,
+    layers: basemap,
+});  
+
+var cities = L.geoJson(loc, {
+     style: function (feature) {
+        return { fillColor: 'rgba(206, 154, 177, 0.63)', fillOpacity: 0.25,
+               color: '#b9015b', weight: 2, opacity: 1};
+    },
+    onEachFeature: function(feature, featureLayer) {
+        featureLayer.bindTooltip(feature.properties.NAME, {permanent: false, direction: 'right'});
+    }
+}).addTo(mymap);
+    
+mymap.fitBounds(cities.getBounds());
+
+var migrationLayer = new L.migrationLayer({
+    map: mymap,
+    data: data,
+    pulseRadius:20,
+    pulseBorderWidth:1,
+    arcWidth:5,
+    arcLabel:false,
+    arcLabelFont:'14px sans-serif',
+    maxWidth:10
+});
+
+migrationLayer.addTo(mymap);
+
+function hide(){
+    migrationLayer.hide();
+}
+function show(){
+    migrationLayer.show();
+}
+function play(){
+    migrationLayer.play();
+}
+function pause(){
+    migrationLayer.pause();
+}
+        
